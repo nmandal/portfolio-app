344491002b27a77fdb46b6328cf63f1fd9ec57f7
nmandal <nicholas.mandal@gmail.com>
2022-02-18 10:16:18 -0600

    add id for sample data

--- data/portfolio.ts
+++ data/portfolio.ts
@@ -1,5 +1,6 @@
 export const portfolios = [
     {
+      "id": '8a400816-9ccf-40c3-9d63-33cb3ca01217',
       "date": "2020-01-01",
       "total": 10340,
       "portfolio": [
@@ -14,6 +15,7 @@ export const portfolios = [
       ]
     },
     {
+      "id": 'bdac3e8f-4196-40de-b79b-d8a24525a277',
       "date": "2020-03-15",
       "total": 12582,
       "portfolio": [
@@ -32,6 +34,7 @@ export const portfolios = [
       ]
     },
     {
+      "id": '35b63612-06f5-4665-9719-b56ed6271478',
       "date": "2020-07-13",
       "total": 15280,
       "portfolio": [
